b1700af5e3bebf505c888aff381d696ec6033761
VivianPita <pita.1948533@studenti.uniroma1.it>
2023-05-20 14:44:25 +0200

    data

--- code/Data.js
+++ code/Data.js
@@ -1,30 +1,96 @@
 let shopItemsData = [
-    {
-      id: "jfhgbvnscs",
-      name: "Crocchette Cani",
-      price: 5,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "crocchette_cani.png",
-    },
-    {
-      id: "ioytrhndcv",
-      name: "Crocchette Gatti",
-      price: 6,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "crocchette_gatti.jpeg",
-    },
-    {
-      id: "wuefbncxbsn",
-      name: "Lettiera Gatti",
-      price: 16,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "lettiera.jpeg",
-    },
-    {
-      id: "thyfhcbcv",
-      name: "Tappetini Cani",
-      price: 15,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "tappetini.jpeg",
-    },
-  ];
+  {
+    id: "dreffr",
+    name: "Crocchette Per Cani",
+    price: 5,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "crocchette_cani.png",
+  },
+  {
+    id: "vegvsd",
+    name: "Crocchette Per Gatti",
+    price: 6,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "crocchette_gatti.jpeg",
+  },
+  {
+    id: "swegrvadf",
+    name: "Cibo Per Pesci",
+    price: 6,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "pesci.jpeg",
+  },
+  {
+    id: "fdsgb",
+    name: "Cibo Per Roditori",
+    price: 4,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "criceti.jpeg",
+  },
+
+  {
+    id: "sgrhdf",
+    name: "Cibo Per Conigli",
+    price: 8,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "conigli.jpeg",
+  },
+
+
+  {
+    id: "asdxhsindj",
+    name: "Gioco Per Gatti",
+    price: 11,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "gioco_gatti.jpeg",
+  },
+
+  {
+    id: "saehous",
+    name: "Guinzaglio Per Cani",
+    price: 10,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "guinzaglio.jpeg",
+  },
+
+  {
+    id: "awioaio",
+    name: "Gioco Per Cani",
+    price: 9,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "palla_cane.jpeg",
+  },
+
+  {
+    id: "dxsiencjslk",
+    name: "Collari Antiparassita Per Gatti",
+    price: 20,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "antiparassita_gatti.jpeg",
+  },
+
+  {
+    id: "sxcjnaeli",
+    name: "Gocce Anti-Zecche Cani",
+    price: 13,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "antiparassita_cani.jpeg",
+  },
+
+  {
+    id: "sadoijk",
+    name: "Lettiera Igienica Per Gatti",
+    price: 16,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "lettiera.jpeg",
+  },
+
+  {
+    id: "aspowdk",
+    name: "Tappetini Igienici Per Cani",
+    price: 15,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "tappetini.jpeg",
+  },
+
+];
