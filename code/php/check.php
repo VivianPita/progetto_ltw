28ebd5e78f90e0174de03c2f4807bcca8b8d49f4
SamueleProietti <116893718+SamueleProietti@users.noreply.github.com>
2023-05-31 00:47:17 +0200

    Update check.php

--- code/php/check.php
+++ code/php/check.php
@@ -7,12 +7,13 @@ $email = $connessione->real_escape_string($_POST["email"]);
 $citta = $connessione->real_escape_string($_POST["citta"]);
 $indirizzo = $connessione->real_escape_string($_POST["indirizzo"]);
 $cap = $connessione->real_escape_string($_POST["cap"]);
-$data = $connessione->real_escape_string($_POST["data"]);
+$data = $connessione->real_escape_string($_POST["dati"]);
 
 
 
 
 $sql = "INSERT INTO ordini (nome, email, citta, indirizzo, cap, data) VALUES ('$nome', '$email', '$citta', '$indirizzo', '$cap', '$data')";
+
 if($connessione->query($sql) === true){
     
     header("location: ../cart.html");
