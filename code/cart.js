ae5372a5441af2cfbb40e08ebc2366abd2b69ffd
SamueleProietti <116893718+SamueleProietti@users.noreply.github.com>
2023-05-31 00:48:09 +0200

    Update cart.js

--- code/cart.js
+++ code/cart.js
@@ -131,20 +131,3 @@ let TotalAmount = () => {
 
 TotalAmount();
 
-var cart_data = localStorage.data;
-
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "php/check.php", true);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    
-    console.log(xhr.responseText);
-    
-  }
-};
-
-xhr.send("data=" + encodeURIComponent(cart_data));
-
-
