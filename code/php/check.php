46ba335b89cd214a3f6760b7a56a5e1693971274
SamueleProietti <116893718+SamueleProietti@users.noreply.github.com>
2023-06-07 15:06:32 +0200

    Update check.php

--- code/php/check.php
+++ code/php/check.php
@@ -9,16 +9,20 @@ $indirizzo = $connessione->real_escape_string($_POST["indirizzo"]);
 $cap = $connessione->real_escape_string($_POST["cap"]);
 $data = $connessione->real_escape_string($_POST["dati"]);
 
-
-
+// Ottiene i valori dei campi del modulo
 
 $sql = "INSERT INTO ordini (nome, email, citta, indirizzo, cap, data) VALUES ('$nome', '$email', '$citta', '$indirizzo', '$cap', '$data')";
 
+// Crea la query SQL per inserire i dati nella tabella "ordini"
+
 if($connessione->query($sql) === true){
-    
+    // Esegui la query e verifica se l'inserimento è avvenuto con successo
+
     header("location: ../cart.html");
+    // Reindirizza l'utente alla pagina del carrello dopo aver completato l'inserimento
 }else{
     echo "Errore durante prenotazione $sql. " . $connessione->error;
+    // Stampa un messaggio di errore in caso di problemi durante l'inserimento dei dati
 }
 
 ?>
