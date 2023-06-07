67926c6299efeb3a980a672f4de8c47aecb22e59
SamueleProietti <116893718+SamueleProietti@users.noreply.github.com>
2023-06-07 15:16:19 +0200

    Update reservation.php

--- code/php/reservation.php
+++ code/php/reservation.php
@@ -7,12 +7,18 @@ $numero = $connessione->real_escape_string($_POST["numero"]);
 $data = $connessione->real_escape_string($_POST["data"]);
 $ora = $connessione->real_escape_string($_POST["ora"]);
 
+// Ottiene i valori dai campi del modulo di prenotazione e li protegge da eventuali attacchi di SQL injection utilizzando il metodo real_escape_string()
 
 $sql = "INSERT INTO prenotazioni (nome, numero, data, ora) VALUES ('$nome', '$numero', '$data', '$ora')";
-if($connessione->query($sql) === true){
+
+// Crea una query per inserire i dati della prenotazione nella tabella "prenotazioni"
+
+if ($connessione->query($sql) === true) {
     header("location: ../conferma.php");
-}else{
-    echo "Errore durante prenotazione $sql. " . $connessione->error;
+    // Se l'inserimento dei dati della prenotazione nella tabella "prenotazioni" ha avuto successo, l'utente viene reindirizzato alla pagina di conferma della prenotazione
+} else {
+    echo "Errore durante la prenotazione $sql. " . $connessione->error;
+    // In caso di errore durante l'inserimento dei dati della prenotazione, viene stampato un messaggio di errore
 }
 
 ?>
