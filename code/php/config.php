597c5eeca17264447da08e8fdd6f2b96d0acc3c0
SamueleProietti <116893718+SamueleProietti@users.noreply.github.com>
2023-06-07 15:08:23 +0200

    Update config.php

--- code/php/config.php
+++ code/php/config.php
@@ -7,8 +7,13 @@ $db = "progetto_ltw-main";
 
 $connessione = new mysqli($host, $user, $password, $db);
 
+// Configurazione delle informazioni di connessione al database
+
 if($connessione === false){
-    die("Errore di connesione: " . $connessione->connect_error);
+    die("Errore di connessione: " . $connessione->connect_error);
 }
+
+// Verifica se la connessione al database è stata stabilita con successo
+// In caso di errore, viene stampato un messaggio di errore e l'esecuzione dello script termina
 
 ?>
