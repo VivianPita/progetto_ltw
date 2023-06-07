bf3c2956272db2992cae49f94a7a3e623c4f298f
SamueleProietti <116893718+SamueleProietti@users.noreply.github.com>
2023-06-07 15:12:32 +0200

    Update logout.php

--- code/php/logout.php
+++ code/php/logout.php
@@ -2,12 +2,22 @@
 
 session_start();
 
+// Avvia la sessione
+
 $_SESSION = array();
+
+// Svuota l'array delle variabili di sessione
 
 session_destroy();
 
+// Distrugge la sessione corrente
+
 header("location: ../log.php");
 
+// Reindirizza l'utente alla pagina di login
+
 exit;
+
+// Termina l'esecuzione dello script
 
 ?>
